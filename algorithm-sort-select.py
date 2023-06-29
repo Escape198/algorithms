1f78c7477ac0ef608517882117891db5a8697a6b
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2023-06-29 17:07:36 +0700

    Update algorithm-sort-select.py

--- algorithm-sort-select.py
+++ algorithm-sort-select.py
@@ -6,16 +6,16 @@ a = [-3, 5, 0, -8, 1, 10]
 N = len(a)   
 
 for i in range(N-1):
-    maximum = a[i]            # запоминаем минимальное значение
-    minimum = i               # запоминаем индекс минимального значения
+    minimum = a[i]            # запоминаем минимальное значение
+    minimum_index = i               # запоминаем индекс минимального значения
     for j in range(i+1, N):  # поиск миимального среди оставшихся элементов
-        if maximum > a[j]:
-            maximum = a[j]
-            minimum = j
+        if minimum > a[j]:
+            minimum = a[j]
+            minimum_index = j
 
-    if minimum != i:          # обмен значениями, если был найден минимальный не в i-й позиции
+    if minimum_index != i:          # обмен значениями, если был найден минимальный не в i-й позиции
         t = a[i]
-        a[i] = a[minimum]
-        a[p] = t
+        a[i] = a[minimum_index]
+        a[minimum_index] = t
 
 print(a)
